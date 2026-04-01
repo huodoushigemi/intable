230d61fa4acefb387d28a5f54d9e9753ea1629f1
huodoushigemi <2629878269@qq.com>
2026-04-01 09:52:50 +0000

    feat: compatible antd@4

--- packages/react/src/plugins/antd.ts
+++ packages/react/src/plugins/antd.ts
@@ -2,21 +2,29 @@ import type { Plugin } from 'intable'
 import type { Editor } from 'intable/plugins/EditablePlugin'
 import { resolveOptions } from 'intable/utils'
 
-import { Checkbox, ColorPicker, DatePicker, Input, InputNumber, Rate, Select, Switch, TimePicker } from 'antd'
+import { Checkbox, ColorPicker, DatePicker, Input, InputNumber, Rate, Select, Switch, TimePicker, version } from 'antd'
 import { useEffect, useRef, useState, createElement as h, type FC } from 'react'
+
 import dayjs from 'dayjs'
+import weekday from 'dayjs/plugin/weekday'
+import localeData from 'dayjs/plugin/localeData'
+// 兼容 antd@4 的 DatePicker
+dayjs.extend(weekday)
+dayjs.extend(localeData)
+
 import { createRoot } from '../utils'
 
 export const AntdPlugin: Plugin = {
   name: 'antd',
   store(store) {
     store.editors.text = editor(Input)
+    store.editors.textarea = editor(Input.TextArea, o => ({ ...o }))
     store.editors.number = editor(InputNumber)
     store.editors.rate = editor(Rate)
     store.editors.switch = editor(Switch)
     store.editors.checkbox = editor(Checkbox, o => ({ ...o, checked: o.value, onChange: e => o.onChange(e.target.checked) }))
     store.editors.color = selector(ColorPicker, o => ({ ...o, onChange: undefined, onChangeComplete: v => o.onChange(v?.toHexString?.() || v) }))
-    store.editors.select = selector(Select)
+    store.editors.select = selector(Select, o => ({ style: { width: '100%' }, ...o }))
     store.editors.date = selector(DatePicker, o => ({ ...o, value: o.value && dayjs(o.value), onChange: (_, v) => o.onChange(v) }))
     store.editors.time = selector(TimePicker, o => ({ ...o, value: o.value && dayjs(o.value, 'HH:mm:ss'), onChange: (_, v) => o.onChange(v) }))
     store.editors.datetime = selector(DatePicker, o => ({ ...o, showTime: true, value: o.value && dayjs(o.value), onChange: (_, v) => o.onChange(v) }))
@@ -31,12 +39,15 @@ export const createEditor = (Comp: FC<any>, opts = v => v, isSelector?: boolean)
   const container = document.createElement('div')
   container.style.display = 'contents'
   const root = createRoot(container)
+
+  const Inputs = [Input, InputNumber, Input.TextArea]
   
-  let currentValue = eventKey || value
+  let currentValue = Inputs.includes(Comp) ? (eventKey || value) : value
   let elRef: any
   
   const EditorComp = () => {
     const [val, setVal] = useState(currentValue)
+    const [searchValue, onSearch] = useState(eventKey)
     const ref = useRef<any>(null)
     
     useEffect(() => {
@@ -50,9 +61,9 @@ export const createEditor = (Comp: FC<any>, opts = v => v, isSelector?: boolean)
       isSelector && !Array.isArray(currentValue) && setTimeout(ok, 100)
     }
     
-    const handleKeyDown = (e: any) => {
+    const handleKeyDown = (e: KeyboardEvent) => {
       e.stopPropagation()
-      if (e.key === 'Enter') ok()
+      if (e.key === 'Enter' && !e.shiftKey) ok()
       if (e.key === 'Escape') cancel()
     }
     
@@ -63,14 +74,24 @@ export const createEditor = (Comp: FC<any>, opts = v => v, isSelector?: boolean)
       onPointerDown: (e: any) => e.stopPropagation(),
       onKeyDown: handleKeyDown,
       open: true,
+
+      // select
+      ...+version.split('.')[0] <= 5
+        ? { showSearch: true, optionFilterProp: 'label', searchValue, onSearch }
+        : {  showSearch: { optionFilterProp: 'label', searchValue, onSearch } },
+
+      ...(col?.enum && { options: resolveOptions(col.enum) }),
+
       // variant: 'borderless',
       // style: { width: '100%', height: '100%' },
-      ...(col?.enum && { options: resolveOptions(col.enum) }),
       ...props,
     }))
   }
   
-  root.render(h(EditorComp))
+  // 延迟渲染以兼容 antd@4 下拉框的定位问题
+  setTimeout(() => {
+    root.render(h(EditorComp))
+  }, 0);
   
   return {
     el: container,
